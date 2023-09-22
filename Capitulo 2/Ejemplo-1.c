f03204a64d672a4a544e1645ff79975d86faf933
Javier Paredes <144551197+Xavier14495@users.noreply.github.com>
2023-09-22 01:12:41 -0400

    Programas del capitulo 2

---
+++ Capitulo 2/Ejemplo-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+
+{
+    float PRO;
+
+    printf("Ingrese promedio: ");
+    scanf("%f", &PRO);
+
+    if (PRO >= 6)
+        printf("\nAprobado");
+
+
+    return 0;
+}
