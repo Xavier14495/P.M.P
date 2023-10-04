c1300008e7024cc0b288880672331098f88bbbcf
Javier Paredes <jvkfjavi@gmail.com>
2023-10-04 16:47:09 -0400

    Capitulo 3 completado, faltan comentarios

---
+++ Capitulo 3/3-Ejemplo_4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int NUM;
+    long CUA, SUC = 0;
+
+    printf("\nIngrese un numero entero -0 para terminar-:\t");
+    scanf("%d", &NUM);
+    
+    while (NUM)
+    {
+        CUA = pow (NUM, 2);
+        
+        printf("%d al cubo es %ld", NUM, CUA);
+        
+        SUC = SUC + CUA;
+        
+        printf("\nIngrese un numero entero -0 para terminar-:\t");
+        scanf("%d", &NUM);
+        
+    }
+    
+    printf("\nLa suma de los cuadrados es %ld", SUC);
+    return 0;
+}
