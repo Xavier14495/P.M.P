90df3a2a2c73c9dd8e67849c657b0810db838d1e
Javier Paredes <jvkfjavi@gmail.com>
2023-11-14 02:57:12 -0400

    Version 3

--- Loteria.c
+++ Loteria.c
@@ -7,11 +7,20 @@ struct Registro
 {
     int numerosAleatorios[3];
     int year, month, day, hour, minute, second;
+    float apuesta;
 };
 
+// Prototipos de funciones
 void historial(struct Registro *registros, int numTiradas);
 void tirar(struct Registro **registros, int *numTiradas);
+void jugar(struct Registro **registros, int *numTiradas);
+void Pale(struct Registro **registros, int *numTiradas);
+void Tripleta(struct Registro **registros, int *numTiradas);
+void Numero(struct Registro **registros, int *numTiradas);
 void clear(struct Registro **registros);
+void guardarDatos(struct Registro *registros, int numTiradas);
+int cargarDatos(struct Registro **registros);
+
 
 int main()
 {
@@ -19,14 +28,19 @@ int main()
     struct Registro *registros = NULL;
     int numTiradas = 0;
 
+    numTiradas = cargarDatos(&registros);
+
     srand((unsigned int)time(NULL));
+
+    // Resto del código...
 
     do
     {
         // Menú de opciones
-        printf("\n0. Salir \n");
-        printf("1. Realizar tirada \n");
+        printf("\n0. Salir\n");
+        printf("1. Realizar tirada\n");
         printf("2. Historial\n");
+        printf("3. Jugar\n");
 
         printf("\nElige una opcion: ");
         scanf("%d", &opcion);
@@ -35,7 +49,7 @@ int main()
         {
         case 0:
             // Salir del programa
-            printf("\nHas elegido salir. Adios.\n");
+            printf("\nAdios y vuelva pronto.\n");
             break;
         case 1:
             // Realizar tirada
@@ -44,16 +58,23 @@ int main()
             break;
         case 2:
             // Ver historial de tiradas
-            printf("\nHistorial\n\n");
+            printf("\nHistorial\n------------------\n");
             historial(registros, numTiradas);
+            break;
+        case 3:
+            // Jugar Pale
+            jugar(&registros, &numTiradas);
             break;
         default:
             // Opción no válida
-            printf("\nOpcion no valida. Por favor, elige una opcion del 0 al 2\n");
+            printf("\nOpcion no valida. Por favor, elige una opcion del 0 al 3\n");
             break;
         }
 
     } while (opcion != 0);
+
+    // Guardar datos antes de salir
+    guardarDatos(registros, numTiradas);
 
     // Liberar memoria antes de salir
     clear(&registros);
@@ -66,10 +87,8 @@ void historial(struct Registro *registros, int numTiradas)
     // Mostrar historial de tiradas
     for (int i = 0; i < numTiradas; i++)
     {
-        printf("Tirada %d - N1: %d %d %d\tFecha y hora: %d-%02d-%02d %02d:%02d:%02d\n", i + 1,
-               registros[i].numerosAleatorios[0], registros[i].numerosAleatorios[1], registros[i].numerosAleatorios[2],
-               registros[i].year, registros[i].month, registros[i].day,
-               registros[i].hour, registros[i].minute, registros[i].second);
+        printf("Tirada %d - N1: %d %d %d\tFecha y hora: %d-%02d-%02d %02d:%02d:%02d\n", i + 1, registros[i].numerosAleatorios[0], registros[i].numerosAleatorios[1], registros[i].numerosAleatorios[2], registros[i].year, registros[i].month, registros[i].day, registros[i].hour, registros[i].minute, registros[i].second);
+        printf("\n-----------------------------------------\n");
     }
 }
 
@@ -91,11 +110,14 @@ void tirar(struct Registro **registros, int *numTiradas)
         printf("Error al asignar memoria para la nueva tirada.\n");
         exit(EXIT_FAILURE);
     }
-    
     else
     {
         *registros = temp;
     }
+
+    // Preguntar al usuario la cantidad que quiere apostar
+    printf("Ingresa la cantidad que quieres apostar en esta tirada: ");
+    scanf("%f", &(*registros)[*numTiradas - 1].apuesta);
 
     // Generar tres números aleatorios
     for (int i = 0; i < 3; i++)
@@ -112,9 +134,156 @@ void tirar(struct Registro **registros, int *numTiradas)
     (*registros)[*numTiradas - 1].second = infoTiempo->tm_sec;
 }
 
+void jugar(struct Registro **registros, int *numTiradas)
+{
+    int opcionJuego;
+    
+    printf("\n------------------------------------------\n");
+    printf("1. Pale\n");
+    printf("2. Tripleta\n");
+    printf("3. Numero Especifico\n");
+    printf("\nElige una opcion para jugar:");
+
+    scanf("%d", &opcionJuego);
+
+    switch (opcionJuego)
+    {
+    case 1:
+        Pale(registros, numTiradas);
+        break;
+    case 2:
+        Tripleta(registros, numTiradas);
+        break;
+    case 3:
+        Numero(registros, numTiradas);
+        break;
+    default:
+        printf("Opcion no valida. Por favor, elige una opcion del 1 al 3\n");
+    }
+}
+
+void Pale(struct Registro **registros, int *numTiradas)
+{
+    // Jugar un Pale (escoge dos números y busca si ha salido en alguna tirada)
+    int numero1, numero2;
+
+    printf("\nIngresa dos numeros para jugar el Pale (ejemplo: 12 34): ");
+    scanf("%d %d", &numero1, &numero2);
+
+    // Buscar en las tiradas anteriores
+    for (int i = 0; i < *numTiradas; i++)
+    {
+        if ((*registros)[i].numerosAleatorios[0] == numero1 && (*registros)[i].numerosAleatorios[1] == numero2)
+        {
+            printf("Felicidades, has ganado con el Pale en la tirada %d. Cantidad ganada: %.2f\n", i + 1, (*registros)[i].apuesta * 2);
+            return;
+        }
+        else if ((*registros)[i].numerosAleatorios[0] == numero2 && (*registros)[i].numerosAleatorios[1] == numero1)
+        {
+            printf("Felicidades, has ganado con el Pale en la tirada %d. Cantidad ganada: %.2f\n", i + 1, (*registros)[i].apuesta * 2);
+            return;
+        }
+    }
+
+    printf("Lo siento, no has ganado con el Pale.\n");
+}
+
+void Tripleta(struct Registro **registros, int *numTiradas)
+{
+    // Jugar una Tripleta (escoge tres números y busca si ha salido en alguna tirada)
+    int numero1, numero2, numero3;
+
+    printf("\nIngresa tres numeros para jugar la Tripleta (ejemplo: 12 34 56): ");
+    scanf("%d %d %d", &numero1, &numero2, &numero3);
+
+    // Buscar en las tiradas anteriores
+    for (int i = 0; i < *numTiradas; i++)
+    {
+        if ((*registros)[i].numerosAleatorios[0] == numero1 && (*registros)[i].numerosAleatorios[1] == numero2 && (*registros)[i].numerosAleatorios[2] == numero3)
+        {
+            printf("Felicidades, has ganado con la Tripleta en la tirada %d. Cantidad ganada: %.2f\n", i + 1, (*registros)[i].apuesta * 3);
+            return;
+        }
+    }
+
+    printf("Lo siento, no has ganado con la Tripleta.\n");
+}
+
+void Numero(struct Registro **registros, int *numTiradas)
+{
+    // Jugar un Número específico (escoge un número y busca si ha salido en alguna tirada)
+    int numero;
+
+    printf("\nIngresa un numero para jugar (ejemplo: 42): ");
+    scanf("%d", &numero);
+
+    // Buscar en las tiradas anteriores
+    for (int i = 0; i < *numTiradas; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if ((*registros)[i].numerosAleatorios[j] == numero)
+            {
+                printf("Felicidades, has ganado con el Numero en la tirada %d. Cantidad ganada: %.2f\n", i + 1, (*registros)[i].apuesta * 1.5);
+                return;
+            }
+        }
+    }
+
+    printf("Lo siento, no has ganado con el Numero.\n");
+}
+
 void clear(struct Registro **registros)
 {
     // Liberar la memoria asignada para registros
     free(*registros);
     *registros = NULL;
+}
+// Función para guardar los datos en el disco
+void guardarDatos(struct Registro *registros, int numTiradas)
+{
+    FILE *archivo = fopen("datos.dat", "wb");
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo para escritura.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < numTiradas; i++)
+    {
+        fwrite(&registros[i], sizeof(struct Registro), 1, archivo);
+    }
+
+    fclose(archivo);
+}
+// Función para cargar los datos desde el disco
+int cargarDatos(struct Registro **registros)
+{
+    FILE *archivo = fopen("datos.dat", "rb");
+    if (archivo == NULL)
+    {
+        printf("No se encontró un archivo existente. Se crearan nuevos datos.\n");
+        return 0;
+    }
+
+    fseek(archivo, 0, SEEK_END);
+    int numTiradas = ftell(archivo) / sizeof(struct Registro);
+    fseek(archivo, 0, SEEK_SET);
+
+    *registros = malloc(sizeof(struct Registro) * numTiradas);
+    if (*registros == NULL)
+    {
+        printf("Error al asignar memoria para cargar los datos.\n");
+        fclose(archivo);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < numTiradas; i++)
+    {
+        fread(&(*registros)[i], sizeof(struct Registro), 1, archivo);
+    }
+
+    fclose(archivo);
+
+    return numTiradas;
 }
